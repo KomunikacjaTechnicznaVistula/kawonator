0431ed1cacbb91b6944dbdc10b9b4c37e38a6862
Daniel Barrio Fierro <dbarr@dolby.com>
2022-01-11 20:20:39 +0100

    Adding docs folder

---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {5:2,1:-1,4:2,9:2,3:1,11:1,2:-1,8:2,10:1,7:2,12:2,6:2,13:2};
+});
