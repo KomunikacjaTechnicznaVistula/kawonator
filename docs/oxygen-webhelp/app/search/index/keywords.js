df7d0c92955877a346ba76a27b070a0f46a68387
db4rr <dbarr@dolby.com>
2022-03-26 16:51:54 +0100

    Final version for 2022

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,15 +1,17 @@
-define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Glossary",p:["p1"]},{w:"Consumer",p:["p2"]},{w:"guide",p:["p2","p3"]},{w:"Maintenance",p:["p3"]},{w:"Recipes",p:["p4"]},{w:"Assembling",p:["p5"]},{w:"CLEANING",p:["p6","p7"]},{w:"THE",p:["p6","p7","p8","p9"]},{w:"Filter",p:["p6"]},{w:"holder",p:["p6"]},{w:"AND",p:["p6","p7"]},{w:"Portafilter",p:["p6"]},{w:"Steam",p:["p7"]},{w:"wand",p:["p7"]},{w:"Frothing",p:["p7"]},{w:"attachment",p:["p7"]},{w:"Operating",p:["p8","p9"]},{w:"Kawonator",p:["p8","p9"]},{w:"KW200",p:["p8"]},{w:"espresso",p:["p8","p9"]},{w:"machine",p:["p8","p9"]},{w:"KW100",p:["p9"]}];
+define(function() {var keywords=[{w:"Glossary",p:["p0"]},{w:"Consumer",p:["p1"]},{w:"guide",p:["p1","p2"]},{w:"Maintenance",p:["p2"]},{w:"Coffees",p:["p3"]},{w:"to",p:["p3"]},{w:"try",p:["p3"]},{w:"Assembling",p:["p4","p5"]},{w:"the",p:["p4","p5","p6","p7","p8","p9","p10","p11"]},{w:"Kawomator",p:["p4","p5","p10","p11"]},{w:"KW100",p:["p4","p6","p8","p11"]},{w:"KW200",p:["p5","p7","p9","p10"]},{w:"Cleaning",p:["p6","p7","p8","p9"]},{w:"filter",p:["p6","p7"]},{w:"holder",p:["p6","p7"]},{w:"and",p:["p6","p7","p8","p9"]},{w:"portafilter",p:["p6","p7"]},{w:"steam",p:["p8","p9"]},{w:"wand",p:["p8","p9"]},{w:"frothing",p:["p8","p9"]},{w:"attachment",p:["p8","p9"]},{w:"Operating",p:["p10","p11"]}];
 var ph={};
-ph["p0"]=[0, 1, 2];
-ph["p1"]=[3];
-ph["p2"]=[4, 5];
-ph["p3"]=[6, 5];
-ph["p4"]=[7];
-ph["p5"]=[8];
-ph["p6"]=[9, 10, 11, 12, 13, 10, 14];
-ph["p7"]=[9, 10, 15, 16, 13, 17, 18];
-ph["p8"]=[19, 10, 20, 21, 22, 23];
-ph["p9"]=[19, 10, 20, 24, 22, 23];
+ph["p0"]=[0];
+ph["p1"]=[1, 2];
+ph["p11"]=[21, 8, 9, 10];
+ph["p2"]=[3, 2];
+ph["p3"]=[4, 5, 6];
+ph["p4"]=[7, 8, 9, 10];
+ph["p5"]=[7, 8, 9, 11];
+ph["p6"]=[12, 8, 10, 13, 14, 15, 16];
+ph["p7"]=[12, 8, 11, 13, 14, 15, 16];
+ph["p8"]=[12, 8, 10, 17, 18, 15, 19, 20];
+ph["p9"]=[12, 8, 11, 17, 18, 15, 19, 20];
+ph["p10"]=[21, 8, 9, 11];
      return {
          keywords: keywords,
          ph: ph
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {3:-1,5:3,2:-1,7:3,6:3,11:2,12:2,4:2,10:3,9:3,13:3,8:3,14:3};
+return {11:1,7:2,13:2,6:2,5:2,9:2,8:2,3:1,12:2,1:-1,4:2,10:1,2:-1};
 });
