253ebdcf112d4d1ac47a4db1b212caed91358be9
db4rr <dbarr@dolby.com>
2022-03-26 16:00:53 +0100

    Publishing Students' work

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Glossary",p:["p1"]},{w:"Consumer",p:["p2"]},{w:"guide",p:["p2","p3"]},{w:"Maintenance",p:["p3"]},{w:"Recipes",p:["p4"]},{w:"Assembling",p:["p5"]},{w:"CLEANING",p:["p6","p7"]},{w:"THE",p:["p6","p7","p8","p9"]},{w:"Filter",p:["p6"]},{w:"holder",p:["p6"]},{w:"AND",p:["p6","p7"]},{w:"Portafilter",p:["p6"]},{w:"Steam",p:["p7"]},{w:"wand",p:["p7"]},{w:"Frothing",p:["p7"]},{w:"attachment",p:["p7"]},{w:"Operating",p:["p8","p9"]},{w:"Kawonator",p:["p8","p9"]},{w:"KW200",p:["p8"]},{w:"espresso",p:["p8","p9"]},{w:"machine",p:["p8","p9"]},{w:"KW100",p:["p9"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3];
+ph["p2"]=[4, 5];
+ph["p3"]=[6, 5];
+ph["p4"]=[7];
+ph["p5"]=[8];
+ph["p6"]=[9, 10, 11, 12, 13, 10, 14];
+ph["p7"]=[9, 10, 15, 16, 13, 17, 18];
+ph["p8"]=[19, 10, 20, 21, 22, 23];
+ph["p9"]=[19, 10, 20, 24, 22, 23];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {3:-1,5:3,2:-1,7:3,6:3,11:2,12:2,4:2,10:3,9:3,13:3,8:3,14:3};
+});
